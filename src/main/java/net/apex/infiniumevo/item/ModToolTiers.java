de906d7c963b3e5f4f3c6ce087d6a75bffe1c543
SRG-Apex <ajbscorpio52@gmail.com>
2023-09-08 19:19:23 -0400

    Added handlers for new mining tiers (T5 through T8) and fixed a floating comma
    AMMENDMENT: Fixed tool tiers so they actually work

---
+++ src/main/java/net/apex/infiniumevo/item/ModToolTiers.java
@@ -0,0 +1,45 @@
+package net.apex.infiniumevo.item;
+import net.apex.infiniumevo.InfiniumEvo;
+import net.apex.infiniumevo.util.ModTags;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
+
+import java.util.List;
+
+public class ModToolTiers {
+    public static Tier T5;
+    public static Tier T6;
+//    public static Tier T7;
+//
+//    public static Tier T8;
+
+
+    static {
+        T5 = TierSortingRegistry.registerTier(
+                new ForgeTier(5, 2500, 80f, 3f, 24,
+                        ModTags.Blocks.NEEDS_T5_TOOL, () -> Ingredient.of(ModItems.CRYSTALITE.get())),
+                new ResourceLocation(InfiniumEvo.MOD_ID, "crystalite"), List.of(Tiers.NETHERITE), List.of());
+
+        T6 = TierSortingRegistry.registerTier(
+                new ForgeTier(6, 5000, 110f, 3f, 24,
+                        ModTags.Blocks.NEEDS_T6_TOOL, () -> Ingredient.of(ModItems.INFINIUM.get())),
+                new ResourceLocation(InfiniumEvo.MOD_ID, "infinium"), List.of(Tiers.NETHERITE), List.of());
+
+//        T7 = TierSortingRegistry.registerTier(
+//                new ForgeTier(7, 10000, 150f, 3f, 24,
+//                        ModTags.Blocks.NEEDS_T5_TOOL, () -> Ingredient.of(ModItems.REALITY.get())),
+//                new ResourceLocation(InfiniumEvo.MOD_ID, "reality"), List.of(Tiers.NETHERITE), List.of());
+//
+//        T8 = TierSortingRegistry.registerTier(
+//                new ForgeTier(8, 100000, 200f, 3f, 24,
+//                        ModTags.Blocks.NEEDS_T5_TOOL, () -> Ingredient.of(ModItems.VOID.get())),
+//                new ResourceLocation(InfiniumEvo.MOD_ID, "void"), List.of(Tiers.NETHERITE), List.of());
+
+
+
+    }
+}
---
+++ src/main/java/net/apex/infiniumevo/util/ModTags.java
@@ -0,0 +1,28 @@
+package net.apex.infiniumevo.util;
+
+import net.apex.infiniumevo.InfiniumEvo;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks{
+        public static final TagKey<Block> NEEDS_T5_TOOL
+                = tag("needs_t5_tool");
+        public static final TagKey<Block> NEEDS_T6_TOOL
+                = tag("needs_t6_tool");
+        public static final TagKey<Block> NEEDS_T7_TOOL
+                = tag("needs_t7_tool");
+        public static final TagKey<Block> NEEDS_T8_TOOL
+                = tag("needs_t8_tool");
+
+        private static TagKey<Block> tag(String name){
+            return BlockTags.create(new ResourceLocation(InfiniumEvo.MOD_ID, name));
+        }
+
+        private static TagKey<Block> forgeTag(String name){
+            return BlockTags.create(new ResourceLocation("forge", name));
+        }
+    }
+}
